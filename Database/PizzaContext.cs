6a1a087ec7e028b3963faeb7ec797df9d087961e
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-15 15:04:33 +0200

    create db connection

---
+++ Database/PizzaContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static.Database
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Pizza> Pizza { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=db-pizza;Integrated Security=True");
+        }
+    }
+}
