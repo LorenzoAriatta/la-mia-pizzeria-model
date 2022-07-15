f51de48dffc1d052f11135ae579b0442f8cc793d
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-15 15:02:34 +0200

    add properties for DB

--- Pizza.cs
+++ Pizza.cs
@@ -1,22 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class Pizza
 {
+    [Key]
+    public int Id { get; set; }
     public string Name { get; set; }
 
+    [Column (TypeName = "text")]
     public string Description { get; set; }
 
     public string Image { get; set; }
 
     public double Price { get; set; }
 
-    public int ID { get; set; }
-    public Pizza(string name, string description, string image, double price, int iD)
+    public Pizza(string name, string description, string image, double price, int id)
     {
         Name = name;
         Description = description;
         Image = image;
         Price = price;
-        ID = iD;    
+        Id = id;
     }
 }
